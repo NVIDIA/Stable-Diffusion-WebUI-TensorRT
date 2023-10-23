7618777d061fe9c56bd2a33e724c1a8f06e9fe9a
lspindler <lspindler@nvidia.com>
2023-10-23 06:36:05 -0700

    Faster and simplified install

--- install.py
+++ install.py
@@ -1,31 +1,52 @@
 import launch
+import sys
 from importlib_metadata import version
 
+python = sys.executable
+
+
 def install():
+    import torch
+
+    if not torch.cuda.is_available():
+        print(
+            "Torch CUDA is not available! Please install Torch with CUDA and try again."
+        )
+        return
+
     if launch.is_installed("tensorrt"):
         if not version("tensorrt") == "9.0.1.post11.dev4":
-            launch.run(["python","-m","pip","uninstall","-y","tensorrt"], "removing old version of tensorrt")
-        
-    
-    if not launch.is_installed("tensorrt"):
-        print("TensorRT is not installed! Installing...")
-        launch.run_pip("install nvidia-cudnn-cu11==8.9.4.25 --no-cache-dir", "nvidia-cudnn-cu11")
-        launch.run_pip("install --pre --extra-index-url https://pypi.nvidia.com tensorrt==9.0.1.post11.dev4 --no-cache-dir", "tensorrt", live=True)
-        launch.run(["python","-m","pip","uninstall","-y","nvidia-cudnn-cu11"], "removing nvidia-cudnn-cu11")
-        
-    if launch.is_installed("nvidia-cudnn-cu11"):
-        if version("nvidia-cudnn-cu11") == "8.9.4.25":
-            launch.run(["python","-m","pip","uninstall","-y","nvidia-cudnn-cu11"], "removing nvidia-cudnn-cu11")
+            print("Removing old TensorRT package and try reinstalling...")
+            launch.run(
+                f'"{python}" -m pip uninstall -y tensorrt',
+                "removing old version of tensorrt",
+            )
 
-    # Polygraphy	
+    if not launch.is_installed("tensorrt"):
+        launch.run_pip(
+            "install --pre --extra-index-url https://pypi.nvidia.com  --no-cache-dir --no-deps tensorrt==9.0.1.post11.dev4",
+            "tensorrt",
+            live=True,
+        )
+
+    # Polygraphy
     if not launch.is_installed("polygraphy"):
         print("Polygraphy is not installed! Installing...")
-        launch.run_pip("install polygraphy --extra-index-url https://pypi.ngc.nvidia.com", "polygraphy", live=True)
-    
+        launch.run_pip(
+            "install polygraphy --extra-index-url https://pypi.ngc.nvidia.com",
+            "polygraphy",
+            live=True,
+        )
+
     # ONNX GS
     if not launch.is_installed("onnx_graphsurgeon"):
         print("GS is not installed! Installing...")
         launch.run_pip("install protobuf==3.20.2", "protobuf", live=True)
-        launch.run_pip('install onnx-graphsurgeon --extra-index-url https://pypi.ngc.nvidia.com', "onnx-graphsurgeon", live=True)
- 
+        launch.run_pip(
+            "install onnx-graphsurgeon --extra-index-url https://pypi.ngc.nvidia.com",
+            "onnx-graphsurgeon",
+            live=True,
+        )
+
+
 install()
